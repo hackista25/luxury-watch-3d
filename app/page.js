02ed7c13e7f494f676aedb59affb9a282244add1
DecodewVed <thakurvedant94@gmail.com>
2026-09-19 22:07:44 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,4 @@
+ "use client";
+import WatchViewport from "./components/WatchViewport";
+import WatchUI from "./components/WatchUI";
+export default function Page(){return <><WatchViewport/><WatchUI/></>}
