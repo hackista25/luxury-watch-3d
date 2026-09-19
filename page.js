b75f7731f901557d0599dd52502713372c0be621
DecodewVed <thakurvedant94@gmail.com>
2026-09-19 22:06:18 +0530

    Delete page.js

--- page.js
+++
@@ -1,4 +0,0 @@
- "use client";
-import WatchViewport from "./components/WatchViewport";
-import WatchUI from "./components/WatchUI";
-export default function Page(){return <><WatchViewport/><WatchUI/></>}
